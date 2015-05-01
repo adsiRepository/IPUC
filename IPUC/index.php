cff727bf69180c4346c17093f69083cace70a710
miguel <miguelon_91@misena.edu.co>
2015-05-01 17:37:01 -0500

    Alojamiento inicial del IPUC a GIT

---
+++ IPUC/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html>
+    
+    
+    <head>
+        <meta charset="utf-8">
+        <link href="source/index.css" rel="stylesheet" type="text/css"/>
+        <title>IPUC</title>
+    </head>
+    
+    
+    
+    
+    <body>
+        
+        <div id="panelSuperior"><h1>IGLESIA PENTECOSTAL UNIDA DE COLOMBIA</h1></div>
+        
+        <div id="menuBar">
+            <ul class="navItems">
+                <li><a href="">item 1</a></li>
+                <li><a href="">item 2</a></li>
+                <li><a href="">item 3</a></li>
+            </ul>
+        </div>
+        
+        <div id="panelCentral"></div>
+        
+        <div id="panelDerecho"></div>
+        
+        <div id="panelIzq"></div>
+        
+        <footer>copyright</footer>
+        
+    </body>
+    
+
+    
+</html>
